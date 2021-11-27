ecf3d56c0e4e1ea74d99cf5deca5676964769087
Nilsen84 <56961087+Nilsen84@users.noreply.github.com>
2021-11-27 18:58:35 +0100

    Added a functional transformer

---
+++ src/main/java/me/onils/nopinnedservers/Transformer.java
@@ -0,0 +1,52 @@
+package me.onils.nopinnedservers;
+
+import org.objectweb.asm.ClassReader;
+import org.objectweb.asm.ClassWriter;
+import org.objectweb.asm.Opcodes;
+import org.objectweb.asm.tree.*;
+
+import java.lang.instrument.ClassFileTransformer;
+import java.lang.instrument.IllegalClassFormatException;
+import java.security.ProtectionDomain;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+public class Transformer implements ClassFileTransformer {
+
+    @Override
+    public byte[] transform(ClassLoader loader, String className, Class<?> classBeingRedefined, ProtectionDomain protectionDomain, byte[] classfileBuffer) throws IllegalClassFormatException {
+        if (classfileBuffer == null || classfileBuffer.length == 0) {
+            return new byte[0];
+        }
+
+        if(className.startsWith("lunar/")){
+            ClassReader cr = new ClassReader(classfileBuffer);
+            if(cr.getSuperName().equals("java/lang/Object") && Arrays.asList(cr.getInterfaces()).contains("java/lang/Runnable")){
+                ClassNode cn = new ClassNode();
+
+                cr.accept(cn, 0);
+
+                for(MethodNode method : cn.methods){
+                    Set<String> stringsToMatch = new HashSet<>();
+                    stringsToMatch.add("versions");
+                    stringsToMatch.add("name");
+
+                    boolean matchesAllStrings = Arrays.stream(method.instructions.toArray())
+                            .filter(LdcInsnNode.class::isInstance)
+                            .map(LdcInsnNode.class::cast)
+                            .map(ldc -> ldc.cst)
+                            .filter(stringsToMatch::remove)
+                            .anyMatch(__ -> stringsToMatch.isEmpty());
+                    if(matchesAllStrings){
+                        method.instructions.insert(new InsnNode(Opcodes.RETURN));
+                        ClassWriter cw = new ClassWriter(cr, ClassWriter.COMPUTE_FRAMES);
+                        cn.accept(cw);
+                        return cw.toByteArray();
+                    }
+                }
+            }
+        }
+        return classfileBuffer;
+    }
+}
