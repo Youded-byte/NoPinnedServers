ac823b56649574b93281a604cc5e3473064cfba9
Nilsen84 <56961087+Nilsen84@users.noreply.github.com>
2021-11-27 18:41:50 +0100

    Initial commit

---
+++ src/main/java/me/onils/nopinnedservers/Agent.java
@@ -0,0 +1,11 @@
+package me.onils.nopinnedservers;
+
+import java.lang.instrument.Instrumentation;
+
+public class Agent {
+    public static void premain(String args, Instrumentation inst){
+/*
+        inst.addTransformer(new Transformer(), true);
+*/
+    }
+}
