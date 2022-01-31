29831487ea0a498517fbea4afdea0811c5c6fca8
Nilsen84 <56961087+Nilsen84@users.noreply.github.com>
2022-01-31 20:06:00 +0100

    Fix for latest lunar update

--- src/main/java/me/onils/nopinnedservers/Transformer.java
+++ src/main/java/me/onils/nopinnedservers/Transformer.java
@@ -13,40 +13,45 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class Transformer implements ClassFileTransformer {
-
     @Override
     public byte[] transform(ClassLoader loader, String className, Class<?> classBeingRedefined, ProtectionDomain protectionDomain, byte[] classfileBuffer) throws IllegalClassFormatException {
         if (classfileBuffer == null || classfileBuffer.length == 0) {
             return new byte[0];
         }
 
-        if(className.startsWith("lunar/")){
-            ClassReader cr = new ClassReader(classfileBuffer);
-            if(cr.getSuperName().equals("java/lang/Object") && Arrays.asList(cr.getInterfaces()).contains("java/lang/Runnable")){
-                ClassNode cn = new ClassNode();
+        if (!className.startsWith("lunar/")) {
+            return classfileBuffer;
+        }
 
-                cr.accept(cn, 0);
+        ClassReader cr = new ClassReader(classfileBuffer);
+        if(Arrays.asList(cr.getInterfaces()).contains("java/util/function/Consumer")){
+            ClassNode cn = new ClassNode();
+            cr.accept(cn, 0);
 
-                for(MethodNode method : cn.methods){
-                    Set<String> stringsToMatch = new HashSet<>();
-                    stringsToMatch.add("versions");
-                    stringsToMatch.add("name");
+            for(MethodNode method : cn.methods){
+                Set<String> stringsToMatch = new HashSet<>();
+                stringsToMatch.add("versions");
+                stringsToMatch.add("name");
 
-                    boolean matchesAllStrings = Arrays.stream(method.instructions.toArray())
-                            .filter(LdcInsnNode.class::isInstance)
-                            .map(LdcInsnNode.class::cast)
-                            .map(ldc -> ldc.cst)
-                            .filter(stringsToMatch::remove)
-                            .anyMatch(__ -> stringsToMatch.isEmpty());
-                    if(matchesAllStrings){
-                        method.instructions.insert(new InsnNode(Opcodes.RETURN));
-                        ClassWriter cw = new ClassWriter(cr, ClassWriter.COMPUTE_FRAMES);
-                        cn.accept(cw);
-                        return cw.toByteArray();
-                    }
+                boolean matchesAllStrings = Arrays.stream(method.instructions.toArray())
+                        .filter(LdcInsnNode.class::isInstance)
+                        .map(LdcInsnNode.class::cast)
+                        .map(ldc -> ldc.cst)
+                        .filter(stringsToMatch::remove)
+                        .anyMatch(__ -> stringsToMatch.isEmpty());
+                if(matchesAllStrings){
+                    method.instructions.clear();
+                    method.localVariables.clear();
+                    method.exceptions.clear();
+                    method.tryCatchBlocks.clear();
+                    method.instructions.add(new InsnNode(Opcodes.RETURN));
+                    ClassWriter cw = new ClassWriter(cr, 0);
+                    cn.accept(cw);
+                    return cw.toByteArray();
                 }
             }
         }
+
         return classfileBuffer;
     }
 }
